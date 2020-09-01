2a13db42c395e9fdfbc9236ce310d327724358a1
Julio Lopez <Juliol2540@gmail.com>
2020-09-01 08:46:33 -0500

    removed unused import

--- app/src/main/java/com/example/myfirstapp/MainActivity.java
+++ app/src/main/java/com/example/myfirstapp/MainActivity.java
@@ -6,8 +6,6 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 
-import static android.provider.AlarmClock.EXTRA_MESSAGE;
-
 public class MainActivity extends AppCompatActivity {
 
     public static String EXTRA_MESSAGE;
